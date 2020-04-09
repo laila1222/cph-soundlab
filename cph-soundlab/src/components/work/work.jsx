3fa4fd96c585e31db3adf7c23de958e52593eba5
Laila1222 <korosililla@gmail.com>
2020-04-09 13:45:22 +0200

    installed and added carousel-modern

--- cph-soundlab/src/components/work/work.jsx
+++ cph-soundlab/src/components/work/work.jsx
@@ -1,16 +1,31 @@
-import React from "react";
-import Carousel from "../carousel/carousel";
+import React, { Component } from "react";
+import CarouselModern from "../carousel/carousel-modern";
+
+
+
 import "./_work.scss";
 
-function Work() {
-  return (
-      <section className="section-work">
+class Work extends Component {
+    constructor(props) {
+        super(props)
+
+        this.state = {
+                 
+        }
+    }
+
+    render() {
+        return (
+            <section className="section-work">
         <div className="u-center-middle " >
           <h2 className="secondary-headline" data-aos='fade-up'>Work</h2>
-          <Carousel />
+          <CarouselModern >
+              
+          </CarouselModern>
         </div>
       </section>
-  );
+        )
+    }
 }
 
-export default Work;
+export default Work
