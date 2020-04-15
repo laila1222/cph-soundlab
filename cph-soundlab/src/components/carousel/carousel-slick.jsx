41c0a8a5e3f302cb9fe6a390f98ccfc4080077f3
Laila1222 <korosililla@gmail.com>
2020-04-15 14:05:42 +0200

    chagned styling of work for new design

--- cph-soundlab/src/components/carousel/carousel-slick.jsx
+++ cph-soundlab/src/components/carousel/carousel-slick.jsx
@@ -16,33 +16,64 @@ import ImagineHero from "../../images/imagine-mod.jpg";
 import RecordReleases from "../../images/Recordreleases.jpg";
 import UnderBroen from "../../images/UnderBron.png";
 
-
-
-
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
-import './_carousel.scss';
+import "./_carousel.scss";
 
-const recordsArray = [Bakerman, DocLounge, HotelBoy, ImagineHero, RecordReleases, UnderBroen, Record1, Record2, Record3, Record4, Record5, Record6, Record7, Record8, Record9]
+const recordsArray = [
+  Bakerman,
+  DocLounge,
+  HotelBoy,
+  ImagineHero,
+  RecordReleases,
+  UnderBroen,
+  Record1,
+  Record2,
+  Record3,
+  Record4,
+  Record5,
+  Record6,
+  Record7,
+  Record8,
+  Record9,
+];
 
 export default function SimpleCarousel() {
-    const renderSlides = () =>
-      recordsArray.map(recordImg => (
-        <div className="carousel__image-container u-margin-bottom-medium">
-          <img className="carousel__image u-margin-bottom-small" src={recordImg} alt="record"/>
+  const renderSlides = () =>
+    recordsArray.map((recordImg) => (
+      <div className="carousel__image-container u-margin-bottom-medium">
+        <a href="#">
+          <img
+            className="carousel__image u-margin-bottom-small"
+            src={recordImg}
+            alt="record"
+          />
+        </a>
+        <div className="carousel__content">
           <h3 className="carousel__title">Title</h3>
-          <p className="carousel__description">Lorem ipsum dolor sit amet consectetur oribus modi accusantium inventore odit mollitia perferendis placeat, irem non inventore saepe explicabo eius!</p>
-          <a href="#" className="carousel__link">More</a>
+          <p className="carousel__description">
+            Lorem ipsum dolor sit amet consectetur oribus modi accusantium
+            inventore odit mollitia perferendis placeat, irem non inventore
+            saepe explicabo eius!
+          </p>
+          <a href="#" className="carousel__link button-text">
+            More &#8594;
+          </a>
         </div>
-      ));
-  
-    return (
-      <div className="carousel" data-aos="fade-up">
-        <Slider dots={true}
+      </div>
+    ));
+
+  return (
+    <div className="carousel" data-aos="fade-up">
+      <Slider
+        dots={true}
         slidesToShow={3}
         slidesToScroll={1}
         autoplay={true}
-        autoplaySpeed={5000}>{renderSlides()}</Slider>
-      </div>
-    );
-  }
+        autoplaySpeed={5000}
+      >
+        {renderSlides()}
+      </Slider>
+    </div>
+  );
+}
