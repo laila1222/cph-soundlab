a93c6959449ee94ae4ea0edbcef7129e3d08fc62
Laila1222 <korosililla@gmail.com>
2020-04-15 14:26:48 +0200

    changed design of studio

--- cph-soundlab/src/components/studio/studio.jsx
+++ cph-soundlab/src/components/studio/studio.jsx
@@ -1,12 +1,23 @@
-import React from 'react';
+import React from "react";
+// Importing images
+import StudioImage1 from '../../images/studio.JPG';
+
+import './_studio.scss';
 
 function Studio() {
-    return (
-        <section className="section-studio">
-            <h2>Studio</h2>
-            
-        </section>
-    )
+  return (
+    <section className="section-studio">
+      <div className="studio__content u-center-middle">
+        <h2 className="secondary-headline u-margin-bottom-medium">Studio</h2>
+        <div className="studio__images-container">
+            <img src={StudioImage1} alt="studio" className="studio__image studio__image--1"/>
+            <img src={StudioImage1} alt="studio" className="studio__image studio__image--2"/>
+            <img src={StudioImage1} alt="studio" className="studio__image studio__image--3"/>
+
+        </div>
+      </div>
+    </section>
+  );
 }
 
 export default Studio;
